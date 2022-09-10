798be1e66bbff1d5c6795d11bac38ec0a1b80312
Abdulloyev Ulug'bek <ulugbekabdulloyev22@gmail.com>
2022-09-10 23:12:47 +0500

    USD and UZS

---
+++ JavaScript/Dollor vs Sum/script.js
@@ -0,0 +1,10 @@
+let a, b, c, d, USD, UZS; 
+
+        a = Number(prompt("1 USD = 10 946,5 UZS"));
+        b = Number(prompt("1 UZS = 0.0000914 USD"));
+
+        c = a * 10946;
+        d = b * 0.0000914;
+
+        USD = alert(a + ' USD =' + c + ' UZS (so`m)' );
+        UZS = alert(a + ' UZS =' + d + ' USD (dollor)' );
